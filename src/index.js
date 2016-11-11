4712b2e16825447c13d7fe7cce889d4727e3538c
Werapat <werapat@gmail.com>
2016-11-11 12:20:16 +0700

    - Fix design

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 //     react-native-loading-spinner-overlay
 //     Copyright (c) 2016- Nick Baugh <niftylettuce@gmail.com>
 //     MIT Licensed
@@ -17,122 +16,123 @@
 
 import React from 'react';
 import {
-  StyleSheet,
-  View,
-  Text,
-  Modal
+    StyleSheet,
+    View,
+    Text,
+    Modal,
+    Platform
 } from 'react-native';
 import SpinKit from 'react-native-spinkit';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  },
-  background: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textContainer: {
-    flex: 1,
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-  },
-  textContent: {
-    top: 80,
-    height: 50,
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+    },
+    background: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textContainer: {
+        flex: 1,
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+    },
+    textContent: {
+        top: 80,
+        height: 50,
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
 });
 
 const SIZES = ['small', 'normal', 'large'];
 
 export default class Spinner extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { visible: this.props.visible, textContent: this.props.textContent };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {visible: this.props.visible, textContent: this.props.textContent};
+    }
 
-  static propTypes = {
-    visible: React.PropTypes.bool,
-    textContent: React.PropTypes.string,
-    color: React.PropTypes.string,
-    size: React.PropTypes.oneOf(SIZES),
-    overlayColor: React.PropTypes.string
-  };
+    static propTypes = {
+        visible: React.PropTypes.bool,
+        textContent: React.PropTypes.string,
+        color: React.PropTypes.string,
+        size: React.PropTypes.oneOf(SIZES),
+        overlayColor: React.PropTypes.string
+    };
 
-  static defaultProps = {
-    visible: false,
-    textContent: "",
-    color: 'white',
-    size: 'large', // 'normal',
-    overlayColor: 'rgba(0, 0, 0, 0.25)'
-  };
+    static defaultProps = {
+        visible: false,
+        textContent: "",
+        color: 'white',
+        size: 'large', // 'normal',
+        overlayColor: 'rgba(0, 0, 0, 0.25)'
+    };
 
-  close() {
-    this.setState({ visible: false });
-  }
+    close() {
+        this.setState({visible: false});
+    }
 
-  componentWillReceiveProps(nextProps) {
-    const { visible, textContent } = nextProps;
-    this.setState({ visible, textContent });
-  }
+    componentWillReceiveProps(nextProps) {
+        const {visible, textContent} = nextProps;
+        this.setState({visible, textContent});
+    }
 
-  _renderSpinner() {
-    const { visible, textContent } = this.state;
+    _renderSpinner() {
+        const {visible, textContent} = this.state;
 
-    if (!visible)
-      return (
-        <View />
-      );
+        if (!visible)
+            return (
+                <View />
+            );
 
-    const spinner = (
-      <View style={styles.container} key={`spinner_${Date.now()}`}>
-        <View
-          style={[
-            styles.background,
-            { backgroundColor: this.props.overlayColor }
-          ]}>
-          <SpinKit
-            type={'Bounce'}
-            color={this.props.color}
-            size={this.props.size}
-            style={{ flex: 1 }}
-            />
-          <View style={styles.textContainer}>
-            <Text style={[styles.textContent, this.props.textStyle]}>{textContent}</Text>
-          </View>
-        </View>
-      </View>
-    );
+        const spinner = (
+            <View style={styles.container} key={`spinner_${Date.now()}`}>
+                <View
+                    style={[
+                        styles.background,
+                        {backgroundColor: this.props.overlayColor, alignItems: 'center', justifyContent: 'center'}
+                    ]}>
+                    <SpinKit
+                        isVisible={true}
+                        type={Platform.OS === 'ios' ? 'Arc' : 'ThreeBounce'}
+                        color={'#FFFFFF'}
+                        size={50}
+                    />
+                    <View style={styles.textContainer}>
+                        <Text style={[styles.textContent, this.props.textStyle]}>{textContent}</Text>
+                    </View>
+                </View>
+            </View>
+        );
 
-    return (
-      <Modal onRequestClose={() => this.close()} visible={visible} transparent>
-        {spinner}
-      </Modal>
-    );
+        return (
+            <Modal onRequestClose={() => this.close()} visible={visible} transparent>
+                {spinner}
+            </Modal>
+        );
 
-  }
+    }
 
-  render() {
-    return this._renderSpinner();
-  }
+    render() {
+        return this._renderSpinner();
+    }
 
 }
